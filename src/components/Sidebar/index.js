a5c18ce03cc4b8125dc6631d0a64bb64b2169d88
venkateshsure <venky007sure@gmai.com>
2024-01-27 05:58:13 +0000

    sidebar is implemented

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { IconButton } from "@material-ui/core";
+
 import "./index.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>sidebar</h1>
+      <div className="sidebar_header">
+        <AccountCircleIcon />
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchIcon />
+          <input
+            className="input"
+            type="text"
+            placeholder="search or start new chat"
+          />
+        </div>
+      </div>
+      <div className="sidebar_chats"></div>
     </div>
   );
 }
