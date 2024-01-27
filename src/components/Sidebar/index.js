643e47449734b249a3204438c84d62949d75d8a5
venkateshsure <venky007sure@gmai.com>
2024-01-27 04:48:47 +0000

    initial ssetup for whatsapp clone

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./index.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZlQZP8KVOZ1QNUS0m7XaQqEZwj3HLmFY",
+  authDomain: "whatsapp-clone-2f943.firebaseapp.com",
+  projectId: "whatsapp-clone-2f943",
+  storageBucket: "whatsapp-clone-2f943.appspot.com",
+  messagingSenderId: "573527895482",
+  appId: "1:573527895482:web:290b4e0a9f998e3ed38183",
+  measurementId: "G-2MZ4NEPMGF",
+};
